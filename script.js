1d0c100c963153eed17df41bd3e388a7da3fba64
Nick <84020126+usalokiN@users.noreply.github.com>
2022-10-19 23:20:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const column = document.querySelectorAll('.services__item');
-const links = document.querySelectorAll('.footer-link');
+window.addEventListener('DOMContentLoaded', () => {
+
+    const column = document.querySelectorAll('.services__item'),
+          links = document.querySelectorAll('.footer-link');
 
 column.forEach(element => {
     element.addEventListener('mouseover', event => {
@@ -29,4 +31,49 @@ links.forEach(e => {
         e.style.transition = "5s else-in-out";
         
     });
+});
+
+//  timer
+    const deadLine = '2022-12-30';
+
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        days = Math.floor (t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor (t / (1000 * 60 * 60) % 24),
+        minutes = Math.floor (t / (1000 / 60) % 60),
+        seconds = Math.floor ((t / 1000) % 60); 
+    
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+        };
+    }
+
+function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds');
+            timeInterval = setInterval((updateClock), 1000);
+
+
+            function updateClock() {
+                const t = getTimeRemaining(endtime);
+                days.innerHTML = (t.days);
+                hours.innerHTML = (t.hours);
+                minutes.innerHTML = (t.minutes);
+                seconds.innerHTML = t.seconds;
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval)
+                }
+            }
+        }
+        setClock('.main-block__timer', deadLine);
 });
